3ff244cc217a06b27569224e6e04aa5cf5d35df2
faf4r <edgecage@foxmail.com>
2024-05-11 20:18:26 +0800

    feat: add cpu usage

--- src/services/messageResponder.ts
+++ src/services/messageResponder.ts
@@ -1,9 +1,27 @@
 import os from 'node:os'
 import type { Room } from 'wechaty'
 
+function getCpuUsage(): number {
+  const cpus = os.cpus()
+
+  // 计算每个CPU核心的使用率
+  const total = cpus.reduce((acc, cpu) => {
+    const total = Object.values(cpu.times).reduce((total, time) => total + time, 0)
+    const idle = cpu.times.idle
+
+    return acc + (1 - idle / total)
+  }, 0)
+
+  // 计算平均CPU使用率
+  const avgCpuUsage = total / cpus.length
+
+  return avgCpuUsage
+}
+
 async function state(room: Room) {
+  const cpu = (getCpuUsage() * 100).toPrecision(3)
   const mem = (100 - os.freemem() / os.totalmem() * 100).toPrecision(4)
-  await room.say(`mem used: ${mem}%\nloadavg: ${os.loadavg()}`)
+  await room.say(`cpu usage: ${cpu}%\nmem used: ${mem}%\nloadavg: ${os.loadavg()}`)
 }
 
 async function act_for(msg: string, room: Room) {
